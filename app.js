a64b2e4b2e896b81da527a87358fdcf07ff039d8
sulih <sulih055@gmail.com>
2023-04-28 16:28:07 +0700

    perbaikan lagi

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-const appRoute = require('./src/routes/route-karyawan');
-app.use('/', appRoute);
-
 app.listen(8080, ()=>{
     console.log('Server Berjalan di Port : 8080');
+
+    
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send({
+        status: "success",
+        data: {
+            message: "API working fine"
+        }
+    });
 });
+
+app.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+        status: "error",
+        message: err.message
+    });
+    next();
+});
+});
+
+module.exports = app;
--- src/routes/route-karyawan.js
+++ src/routes/route-karyawan.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const { karyawan } = require('../controllers');
+const { asyncHandler } = require('../middlewares/asyncHandler');
+const checkEmail = require('../middlewares/checkEmail');
+const { signup: signupValidator, signin: signinValidator } = require('../validators/auth');
+const authController = require('../controllers/auth.controller');
+
 
 router.get('/karyawan', karyawan.getDataKaryawan);
 router.get('/karyawan/:id', karyawan.getDataKaryawanByID);
 router.post('/karyawan/add', karyawan.addDataKaryawan);
 router.post('/karyawan/edit', karyawan.editDataKaryawan);
 router.post('/karyawan/delete/', karyawan.deleteDataKaryawan);
+
+router.route('/signup')
+    .post(signupValidator, asyncHandler(checkEmail), asyncHandler(authController.signup));
+
+router.route('/signin')
+    .post(signinValidator, asyncHandler(authController.signin));
 
 module.exports = router;
